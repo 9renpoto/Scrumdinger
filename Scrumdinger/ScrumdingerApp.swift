a3ffb2b4e7fa4de64a4d026b7e319a8f7b5c493a
Keisuke Umeno <9renpoto@gmail.com>
2024-05-31 08:52:26 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Keisuke Umeno on 2024/05/31.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
